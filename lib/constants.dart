da2855f99ce12ffdc6da9faf42d0476c60a4bbd3
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-28 14:27:55 +0500

    creating reusable components

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color colorgrey = Colors.grey;
+
+const TextStyle headingTextStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
+
+
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator_app/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            MyButton(),
+            MyButton(),
+            MyButton(),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-        ],
+class MyButton extends StatefulWidget {
+  const MyButton({super.key});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0), // Add padding here
+        child: Container(
+          height: 50,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.deepOrange,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(child: Text('Login', style: headingTextStyle)),
+        ),
       ),
     );
   }
