60ede37355e1b22f65f60139b30bb0dbf582c2c4
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-30 12:40:05 +0500

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calculator_app/components/my_button.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
+   HomeScreen({super.key});
+var userInput = '';
+var answer = '';
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -14,34 +15,113 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
           child: Column(
+
             children: [
-              Row(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // Align buttons evenly
+
                 children: [
-                  MyButton(title: 'AC',onPress: (){
-                    print('tap');
-                  },),
-                  SizedBox(width: 10,),
-                  MyButton(title: '+/-', onPress: (){
-                    print('tap');
-                  },),
-                  SizedBox(width: 10, ),
-                  MyButton(title: '%', onPress: (){
-                    print('tap');
-                  },),
-                  SizedBox(width: 10,),
-                  MyButton(title: '/',color: Color(0xffffa00a), onPress: (){
-                    print('tap');
-                  },),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // Align buttons evenly
+                    children: [
+                      MyButton(title: 'AC',onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '+/-', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10, ),
+                      MyButton(title: '%', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '/',color: Color(0xffffa00a), onPress: (){
+                        print('tap');
+                      },),
 
 
 
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // Align buttons evenly
+                    children: [
+                      MyButton(title: 'AC',onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '+/-', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10, ),
+                      MyButton(title: '%', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '/',color: Color(0xffffa00a), onPress: (){
+                        print('tap');
+                      },),
+
+
+
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // Align buttons evenly
+                    children: [
+                      MyButton(title: 'AC',onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '+/-', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10, ),
+                      MyButton(title: '%', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '/',color: Color(0xffffa00a), onPress: (){
+                        print('tap');
+                      },),
+
+
+
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // Align buttons evenly
+                    children: [
+                      MyButton(title: 'AC',onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '+/-', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10, ),
+                      MyButton(title: '%', onPress: (){
+                        print('tap');
+                      },),
+                      SizedBox(width: 10,),
+                      MyButton(title: '/',color: Color(0xffffa00a), onPress: (){
+                        print('tap');
+                      },),
+
+
+
+                    ],
+                  ),
+                  SizedBox(height: 10),
                 ],
               ),
-              SizedBox(height: 10),
             ],
           ),
         ),
